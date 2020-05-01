fa234eeb342c5f21a3c9b66ae64d6ddf1db08f05
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-01 16:42:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-    if brand == 3
-    end
+    BRANDS << brand unless BRANDS.include? (brand)
+    
   end
 
   def cobble
